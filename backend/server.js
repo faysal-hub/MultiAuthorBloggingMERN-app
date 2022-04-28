8ac64f539a11a6a3e699690af51273f32d499a1f
faysalwzbm <ovitahsan@gmail.com>
2022-04-28 23:59:54 +0200

    import the dotenv file & configer the GET request

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const dotEnv = require('dotenv');
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req,res) =>{
+    res.send('server is running!')
+})
+
+dotEnv.config({
+    path : 'config/config.env'
+})
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, (error)=>{
     if(!error){
