11b337370f35b2cb920b02dd5463f661f3288812
faysalwzbm <ovitahsan@gmail.com>
2022-05-01 18:34:23 +0200

    frontend: design the Navbar

--- frontend/src/components/home/Navbar.js
+++ frontend/src/components/home/Navbar.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
 
 const Navbar = () => {
-    return <button class="btn">Button</button>;
-}
+  return (
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
+        <a href="/">
+          <img
+            className="w-44 object-contain cursor-pointer"
+            src="/01.png"
+            alt="logo"
+          />
+        </a>
+        <div className="hidden md:inline-flex items-center space-x-5">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
+            Follow
+          </h3>
+        </div>
+      </div>
+
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3> Sign In</h3>
+        <h3 className="border px-4 py-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
