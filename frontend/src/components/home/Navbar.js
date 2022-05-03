524d93affba659d477e84b11de1a61b18b8289a9
faysalwzbm <ovitahsan@gmail.com>
2022-05-03 20:38:09 +0200

    Frontend: change a tag to Link

--- frontend/src/components/home/Navbar.js
+++ frontend/src/components/home/Navbar.js
@@ -1,30 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <header className="flex justify-between p-5 max-w-7xl mx-auto">
       <div className="flex items-center space-x-5">
-        <a href="/">
+        <Link className="image" to="/">
           <img
             className="w-44 object-contain cursor-pointer"
             src="/01.png"
             alt="logo"
           />
-        </a>
-        <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="text-white bg-green-600 px-4 py-1 rounded-full">
-            Follow
-          </h3>
+        </Link>
+        <div className="hidden md:inline-flex items-center space-x-5 cursor-pointer">
+          <Link to="/about">
+            <h3>About</h3>
+          </Link>
+          <Link to="/contact">
+            <h3>Contact</h3>
+          </Link>
+          <Link to="/follow">
+            <h3 className="text-white bg-green-600 px-4 py-1 rounded-full hover:bg-green-700">
+              Follow
+            </h3>
+          </Link>
         </div>
       </div>
 
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3> Sign In</h3>
-        <h3 className="border px-4 py-1 rounded-full border-green-600">
-          Get Started
-        </h3>
+      <div className="flex items-center space-x-5 text-green-600 cursor-pointer">
+        <Link to="/signIn">
+          <h3> Sign In</h3>
+        </Link>
+        <Link to="/signUp">
+          <h3 className="border px-4 py-1 rounded-full border-green-600 hover:bg-green-700 hover:text-white">
+            Get Started
+          </h3>
+        </Link>
       </div>
     </header>
   );
