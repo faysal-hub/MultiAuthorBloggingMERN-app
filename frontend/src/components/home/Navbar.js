13f7b817d1df7c9603b54259b54f527427df8604
faysalwzbm <ovitahsan@gmail.com>
2022-05-03 21:10:12 +0200

    Frontend: fix bug of the NavBar

--- frontend/src/components/home/Navbar.js
+++ frontend/src/components/home/Navbar.js
@@ -12,27 +12,27 @@ const Navbar = () => {
             alt="logo"
           />
         </Link>
-        <div className="hidden md:inline-flex items-center space-x-5 cursor-pointer">
+        <div className="hidden md:inline-flex items-center space-x-5">
           <Link to="/about">
-            <h3>About</h3>
+            <h3 className="cursor-pointer">About</h3>
           </Link>
           <Link to="/contact">
-            <h3>Contact</h3>
+            <h3 className="cursor-pointer">Contact</h3>
           </Link>
           <Link to="/follow">
-            <h3 className="text-white bg-green-600 px-4 py-1 rounded-full hover:bg-green-700">
+            <h3 className="text-white bg-green-600 px-4 py-1 rounded-full hover:bg-green-700 cursor-pointer">
               Follow
             </h3>
           </Link>
         </div>
       </div>
 
-      <div className="flex items-center space-x-5 text-green-600 cursor-pointer">
+      <div className="flex items-center space-x-5 text-green-600">
         <Link to="/signIn">
-          <h3> Sign In</h3>
+          <h3 className="cursor-pointer"> Sign In</h3>
         </Link>
         <Link to="/signUp">
-          <h3 className="border px-4 py-1 rounded-full border-green-600 hover:bg-green-700 hover:text-white">
+          <h3 className="border px-4 py-1 rounded-full border-green-600 hover:bg-green-700 hover:text-white cursor-pointer">
             Get Started
           </h3>
         </Link>
